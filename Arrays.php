58aeb3c0e088e65071a4af4e40a343f5cbc23e43
Shahzad Bashir <shahzadbirmani8@gmail.com>
2020-07-02 00:55:06 +0500

    A Program For Associative Arrays in PHP

--- Arrays.php
+++ Arrays.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /* Numeric array − An array with a numeric index. Values are stored and accessed in linear fashion*/
@@ -50,5 +51,64 @@ for ($x = 0; $x < sizeof($numericArrays2); $x++)
  {
      echo "Value is "."$numericArrays2[$x]"." <br />";
  }
+
+ ////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* Associative array − An array with strings as index. This stores element values in association with key values rather than in a strict linear index order*/
+
+echo "</br>"."Associative array − An array with strings as index. This stores element values in association with key values rather than in a strict linear index order"."</br></br>";
+
+//.....First Method
+
+$AssociativeArrays1=array("2011"=>"batch-1","2012"=>"batch-2","2013"=>"batch-3","2014"=>"batch-4","2015"=>"batch-5");
+
+//.....Second Method
+
+$AssociativeArrays2['2016']="batch-6";
+$AssociativeArrays2['2017']="batch-7";
+$AssociativeArrays2['2018']="batch-8";
+$AssociativeArrays2['2019']="batch-9";
+$AssociativeArrays2['2020']="batch-10";
+
+//.....Displaying Values as Index Values
+
+echo "Value Of Array 1 at Key 2011 = ".$AssociativeArrays1['2011']."</br></br>";
+echo "Value Of Array 2 at Key 2016 = ".$AssociativeArrays2["2016"]."</br></br>";
+
+//.....Displaying Values Using Loops(For & Foreach)
+
+echo "Displaying Array 1 Values Using Foraech"."</br></br>";
+
+foreach($AssociativeArrays1 as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+  }
+
+echo "</br>"."Displaying Array 2 Values Using Foraech"."</br></br>";
+
+foreach($AssociativeArrays2 as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+  }
+
+
+echo "</br>"."Displaying Array 1 Values Using For"."</br></br>";
+
+$keys1=array_keys($AssociativeArrays1);
+
+  for ($x = 0; $x < sizeof($AssociativeArrays1); $x++) 
+   {
+       echo "Key= ".$keys1[$x]." Value= ".$AssociativeArrays1[$keys1[$x]]." <br />";
+   }
+  
+echo "</br>"."Displaying Array 2 Values Using For"."</br></br>";
+
+$keys2=array_keys($AssociativeArrays2);
+  
+for ($x = 0; $x < sizeof($AssociativeArrays2); $x++) 
+{
+    echo "Key= ".$keys2[$x]." Value= ".$AssociativeArrays2[$keys2[$x]]." <br />";
+}
+
 
 ?>
