0b7b8845f3092b102973859eaf324ddd7bd8ff92
Shahzad Bashir <shahzadbirmani8@gmail.com>
2020-07-03 08:48:25 +0500

    Some Examples Of Built-In Functions

--- ArraysFunctions.php
+++ ArraysFunctions.php
@@ -1,3 +1,33 @@
 <?php
+/* Php-Arrays Built-in Function */
+
+//sizeof($arr)
+
+$lamborghinis = array("Urus", "Huracan", "Aventador");
+echo "Size of the array is: ". sizeof($lamborghinis);
+
+//is_array($arr)
+
+$lamborghinis = array("Urus", "Huracan", "Aventador");
+
+// using ternary operator
+echo is_array($lamborghinis) ? 'Array' : 'not an Array';
+
+$mycar = "Urus";
+
+// using ternary operator
+echo is_array($mycar) ? 'Array' : 'not an Array';
+
+
+
+//in_array($var, $arr)
+
+
+$lamborghinis = array("Urus", "Huracan", "Aventador");
+
+// new concept car by lamborghini
+$concept = "estoque";
+
+echo in_array($concept, $lamborghinis) ? 'Added to the Lineup' : 'Not yet!'
 
 ?>
